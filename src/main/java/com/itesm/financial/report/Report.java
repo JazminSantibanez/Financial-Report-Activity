d3daee10135c52412a86a253edc063fa3c877f84
JazminSantibanez <A01234029@itesm.mx>
2022-05-09 04:40:52 -0500

    Update Report.java

--- src/main/java/com/itesm/financial/report/Report.java
+++ src/main/java/com/itesm/financial/report/Report.java
@@ -5,6 +5,10 @@ import java.util.List;
 import com.itesm.financial.Ride;
 
 public abstract class Report {
+
+    /* Open Close 
+        When abstracting the report class, it is easier to implement a new type of report that manages a different print type.
+    */
     
     public abstract String createContent(List<Ride> rides);
     public abstract void createFile(String content) throws IOException;
